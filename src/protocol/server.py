3b16ac0f6bd2b39535eb31995a9d7e2a1111f264
Noelle Floyd <noefloy@users.noreply.github.com>
2022-12-06 13:07:52 -0500

    testing

--- src/protocol/server.py
+++ src/protocol/server.py
@@ -5,9 +5,23 @@ import time
 import srp
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey
+from cryptography.hazmat.backends import default_backend
 
 from packet import Flags
 import packet_manager
+from argparse import ArgumentParser
+import socket, string
+import random, os
+import packet, packet_manager
+from os.path import exists
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric.rsa import RSAPublicKey, RSAPrivateKey
+from cryptography.hazmat.primitives.asymmetric import ec, rsa, padding
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.primitives.serialization import Encoding
+from cryptography.hazmat.primitives.serialization import PublicFormat
+from cryptography.hazmat.primitives.serialization import PrivateFormat
 
 
 class Server:
@@ -80,10 +94,30 @@ class Server:
             #     self.pm.queue(data=str, flag=None, uid=user[1])
             #     self.connections[cid] = {cid, (user[0], user[1])}
 
+def check_fp(pub):
+    if not exists(pub):
+        return False
+    return True
+
 
 if __name__ == "__main__":
-    with open("serve_priv.txt", "rb") as ff:
-        rsa_priv = serialization.load_pem_private_key(ff.read(), password=None,)
+    parser = ArgumentParser()
+    parser.add_argument("-key", type=lambda x: check_fp(x), required=True)
 
+    args = parser.parse_args()
+    with open(args.key, "rb") as ff:
+        if ff.lower().endswith('.der'):
+             rsa_priv = serialization.load_der_public_key(
+                ff.read(),
+                backend=default_backend()
+             )
+        else: 
+            try:
+                rsa_priv = serialization.load_pem_public_key(
+                    ff.read(),
+                    backend=default_backend()
+            )
+            except:
+                print("Error: receiver key file type not .PEM or .DER")
     server = Server(rsa_priv)
     server.run()
